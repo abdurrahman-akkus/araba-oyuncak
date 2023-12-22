5716ce392f5aa310d219d3e59d196cc0725572cf
Algoritim Bilişim <iletisim@algoritimbilisim.com>
2023-12-22 17:07:52 +0300

    Oyun başlasın

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,31 +1,29 @@
 module.exports = {
-  root: true,
-  env: {
-    node: true
-  },
-  'extends': [
-    'plugin:vue/vue3-essential',
-    'eslint:recommended',
-    '@vue/typescript/recommended'
-  ],
-  parserOptions: {
-    ecmaVersion: 2020
-  },
-  rules: {
-    'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
-    'vue/no-deprecated-slot-attribute': 'off',
-    '@typescript-eslint/no-explicit-any': 'off',
-  },
-  overrides: [
-    {
-      files: [
-        '**/__tests__/*.{j,t}s?(x)',
-        '**/tests/unit/**/*.spec.{j,t}s?(x)'
-      ],
-      env: {
-        jest: true
-      }
+    "env": {
+        "browser": true,
+        "es2021": true
+    },
+    "extends": "plugin:vue/vue3-essential",
+    "overrides": [
+        {
+            "env": {
+                "node": true
+            },
+            "files": [
+                ".eslintrc.{js,cjs}"
+            ],
+            "parserOptions": {
+                "sourceType": "script"
+            }
+        }
+    ],
+    "parserOptions": {
+        "ecmaVersion": "latest",
+        "sourceType": "module"
+    },
+    "plugins": [
+        "vue"
+    ],
+    "rules": {
     }
-  ]
 }
--- src/views/HomePage.vue
+++ src/views/HomePage.vue
@@ -1,34 +1,60 @@
 <template>
   <ion-page>
-    <ion-header :translucent="true">
-      <ion-toolbar>
-        <ion-title>Blank</ion-title>
-      </ion-toolbar>
-    </ion-header>
 
-    <ion-content :fullscreen="true">
-      <ion-header collapse="condense">
-        <ion-toolbar>
-          <ion-title size="large">Blank</ion-title>
-        </ion-toolbar>
-      </ion-header>
-
-      <div id="container">
-        <strong>Ready to create an app?</strong>
-        <p>Start with Ionic <a target="_blank" rel="noopener noreferrer" href="https://ionicframework.com/docs/components">UI Components</a></p>
+    <ion-content>
+      <div class="background-image">
+        <div id="container">
+          <div style="display: flex;">
+            <button class="btn" @click="playSound('start3')">Çalıştır</button>
+            <button class="btn" @click="playSound('kapi1')">Kapı</button>
+            <button class="btn" @click="playSound('gec')">Bas Gaza</button>
+            <button class="btn" @click="playSound('skid1')">Ani Fren</button>
+            <button class="btn" @click="playSound('fren')">Fren</button>
+            <button class="btn" @click="playSound('el_freni')">El Freni</button>
+          </div>
+        </div>
+        <div style="display: flex;">
+          <button class="btn" @click="playSound('hadi_gidelim')">Hadi Gidelim</button>
+          <button class="btn" @click="playSound('kirmizi')">Kırmızı</button>
+          <button class="btn" @click="playSound('sari')">Sarı</button>
+          <button class="btn" @click="playSound('yesil')">Yeşil</button>
+        </div>
       </div>
     </ion-content>
   </ion-page>
 </template>
 
-<script setup lang="ts">
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/vue';
+<script>
+import { IonPage } from '@ionic/vue';
+export default {
+  methods: {
+    playSound(sound) {
+      const audio = new Audio('/sound/' + sound + '.mp3'); // Sesi yükleyin
+      audio.play(); // Sesi çalın
+    }
+  }
+}
 </script>
 
 <style scoped>
+.background-image {
+  background-size: cover;
+  background-position: center center;
+  background-image: url('/public/img/direksiyon.jpg');
+  height: 100vh;
+}
+
+.btn {
+  width: 9rem;
+  height: 6rem;
+  margin: 1rem;
+  border-radius: 1rem;
+  background: #038574;
+}
+
 #container {
   text-align: center;
-  
+
   position: absolute;
   left: 0;
   right: 0;
@@ -44,9 +70,9 @@ import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/vue
 #container p {
   font-size: 16px;
   line-height: 22px;
-  
+
   color: #8c8c8c;
-  
+
   margin: 0;
 }
 
